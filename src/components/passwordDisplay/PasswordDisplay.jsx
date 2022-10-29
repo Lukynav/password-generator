a5eb3cde5a0c6fcd694cbd3d3612f7a1be4b717e
Lucas Navarro <Lucasnav6@gmail.com>
2022-10-29 14:40:43 -0300

    💄 improve the password display user experience

--- src/components/passwordDisplay/PasswordDisplay.jsx
+++ src/components/passwordDisplay/PasswordDisplay.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { updatePassword } from '../../redux/slices/password'
+import { BiError } from "react-icons/bi";
 
 export const PasswordDisplay = () => {
   const dispatch = useDispatch()
@@ -9,6 +10,15 @@ export const PasswordDisplay = () => {
   useEffect(() => {
     dispatch(updatePassword({ length, parameters }))
   }, [length, parameters])
+
+  if(!parameters.uppercase && !parameters.lowercase && !parameters.number && !parameters.symbol){
+    return(
+      <section className='rounded-md border-2 p-1 border-red-400 bg-red-200 flex gap-1 mb-8'>
+        <BiError color='black' size={24} />
+        <input value='Please, select one option' type='text' className='bg-transparent px-1 text-black w-full' disabled />
+      </section>
+    )
+  }
 
   return (
     <section className='rounded-md border-2 p-1 border-gray-400 flex gap-1 mb-8'>
